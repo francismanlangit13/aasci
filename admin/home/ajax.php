ea5119c4d311133ce05c703cdda23c8a648456b0
francismanlangit13 <francismanlangit13@gmail.com>
2023-09-21 22:20:48 -0700

    fix users

--- admin/home/ajax.php
+++ admin/home/ajax.php
@@ -1,108 +1,172 @@
 <?php
-    if (!defined('DB_SERVER')) {
-        include ('../includes/authentication.php');
-        $user_id = $_SESSION['auth_user']['user_id'];
-        // DB connection parameters
-        $host = DB_SERVER;
-        $user = DB_USERNAME;
-        $password = DB_PASSWORD;
-        $db = DB_NAME;
+   // -------------------------------- Authentication -------------------------------- //
+   if (!defined('DB_SERVER')) {
+      include ('../includes/authentication.php');
+      $user_id = $_SESSION['auth_user']['user_id'];
+      // DB connection parameters
+      $host = DB_SERVER;
+      $user = DB_USERNAME;
+      $password = DB_PASSWORD;
+      $db = DB_NAME;
 
-        $dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
-        try {
+      $dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+      try {
             $conn = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-        } catch (PDOException $e) {
+      } catch (PDOException $e) {
             echo $e->getMessage();
-        }
-    }
+      }
+   }
+   // -------------------------------- DataTable User -------------------------------- //
+   if (isset($_POST["user_list"])){
+      // Reading value
+      $draw = $_POST['draw'];
+      $row = $_POST['start'];
+      $rowperpage = $_POST['length']; // Rows display per page
+      $columnIndex = $_POST['order'][0]['column']; // Column index
+      $columnName = $_POST['columns'][$columnIndex]['data']; // Column name
+      $columnSortOrder = $_POST['order'][0]['dir']; // asc or desc
+      $searchValue = $_POST['search']['value']; // Search value
 
-    // Reading value
-   $draw = $_POST['draw'];
-   $row = $_POST['start'];
-   $rowperpage = $_POST['length']; // Rows display per page
-   $columnIndex = $_POST['order'][0]['column']; // Column index
-   $columnName = $_POST['columns'][$columnIndex]['data']; // Column name
-   $columnSortOrder = $_POST['order'][0]['dir']; // asc or desc
-   $searchValue = $_POST['search']['value']; // Search value
+      $searchArray = array();
 
-   $searchArray = array();
+      // Search
+      $searchQuery = " ";
+      if($searchValue != ''){
+         $searchQuery = " AND (user_id LIKE :user_id OR 
+            fname LIKE :fname OR
+            mname LIKE :mname OR 
+            lname LIKE :lname OR
+            gender LIKE :gender OR
+            birthday LIKE :birthday OR
+            civil_status LIKE :civil_status OR
+            email LIKE :email OR
+            phone LIKE :phone OR
+            user_type_id LIKE :user_type_id OR
+            user_status_id LIKE :user_status_id) ";
+         $searchArray = array( 
+            'user_id'=>"%$searchValue%",
+            'fname'=>"%$searchValue%",
+            'mname'=>"%$searchValue%",
+            'lname'=>"%$searchValue%",
+            'gender'=>"%$searchValue%",
+            'birthday'=>"%$searchValue%",
+            'civil_status'=>"%$searchValue%",
+            'email'=>"%$searchValue%",
+            'phone'=>"%$searchValue%",
+            'user_type_id'=>"%$searchValue%",
+            'user_status_id'=>"%$searchValue%"
+         );
+      }
 
-   // Search
-   $searchQuery = " ";
-   if($searchValue != ''){
-      $searchQuery = " AND (user_id LIKE :user_id OR 
-           fname LIKE :fname OR
-           mname LIKE :mname OR 
-           lname LIKE :lname OR
-           gender LIKE :gender OR
-           birthday LIKE :birthday OR
-           civil_status LIKE :civil_status OR
-           email LIKE :email OR
-           phone LIKE :phone OR
-           user_type_id LIKE :user_type_id OR
-           user_status_id LIKE :user_status_id OR) ";
-      $searchArray = array( 
-           'user_id'=>"%$searchValue%",
-           'fname'=>"%$searchValue%",
-           'mname'=>"%$searchValue%",
-           'lname'=>"%$searchValue%",
-           'gender'=>"%$searchValue%",
-           'birthday'=>"%$searchValue%",
-           'civil_status'=>"%$searchValue%",
-           'email'=>"%$searchValue%",
-           'phone'=>"%$searchValue%",
-           'user_type_id'=>"%$searchValue%",
-           'user_status_id'=>"%$searchValue%"
+      // Total number of records without filtering
+      $stmt = $conn->prepare("SELECT COUNT(*) AS allcount FROM user ");
+      $stmt->execute();
+      $records = $stmt->fetch();
+      $totalRecords = $records['allcount'];
+
+      // Total number of records with filtering
+      $stmt = $conn->prepare("SELECT COUNT(*) AS allcount FROM user WHERE 1 ".$searchQuery);
+      $stmt->execute($searchArray);
+      $records = $stmt->fetch();
+      $totalRecordwithFilter = $records['allcount'];
+
+      // Fetch records
+      $stmt = $conn->prepare("SELECT *, DATE_FORMAT(birthday, '%m-%d-%Y') as newbirthday FROM user WHERE 1 ".$searchQuery." ORDER BY ".$columnName." ".$columnSortOrder." LIMIT :limit,:offset");
+
+      // Bind values
+      foreach ($searchArray as $key=>$search) {
+         $stmt->bindValue(':'.$key, $search,PDO::PARAM_STR);
+      }
+
+      $stmt->bindValue(':limit', (int)$row, PDO::PARAM_INT);
+      $stmt->bindValue(':offset', (int)$rowperpage, PDO::PARAM_INT);
+      $stmt->execute();
+      $empRecords = $stmt->fetchAll();
+
+      $data = array();
+
+      foreach ($empRecords as $row) {
+         $data[] = array(
+            "user_id"=>$row['user_id'],
+            "fname"=>$row['fname'] . ' '. $row['mname']. $row['lname'],
+            "gender"=>$row['gender'],
+            "newbirthday"=>$row['newbirthday'],
+            "civil_status"=>$row['civil_status'],
+            "email"=>$row['email'],
+            "phone"=>$row['phone'],
+            "user_type_id"=>$row['user_type_id'],
+            "user_status_id"=>$row['user_status_id']
+         );
+      }
+
+      // Response
+      $response = array(
+         "draw" => intval($draw),
+         "iTotalRecords" => $totalRecords,
+         "iTotalDisplayRecords" => $totalRecordwithFilter,
+         "aaData" => $data
       );
+
+      echo json_encode($response);
    }
-
-   // Total number of records without filtering
-   $stmt = $conn->prepare("SELECT COUNT(*) AS allcount FROM user ");
-   $stmt->execute();
-   $records = $stmt->fetch();
-   $totalRecords = $records['allcount'];
-
-   // Total number of records with filtering
-   $stmt = $conn->prepare("SELECT COUNT(*) AS allcount FROM user WHERE 1 ".$searchQuery);
-   $stmt->execute($searchArray);
-   $records = $stmt->fetch();
-   $totalRecordwithFilter = $records['allcount'];
-
-   // Fetch records
-   $stmt = $conn->prepare("SELECT * FROM user WHERE 1 ".$searchQuery." ORDER BY ".$columnName." ".$columnSortOrder." LIMIT :limit,:offset");
-
-   // Bind values
-   foreach ($searchArray as $key=>$search) {
-      $stmt->bindValue(':'.$key, $search,PDO::PARAM_STR);
+   // -------------------------------- Add User -------------------------------- //
+   if (isset($_POST["add_user"])) {
+      // Include your database connection code here (e.g., $con)
+      
+      // Validate and sanitize user inputs
+      $fname = mysqli_real_escape_string($con, $_POST['fname']);
+      $mname = mysqli_real_escape_string($con, $_POST['mname']);
+      $lname = mysqli_real_escape_string($con, $_POST['lname']);
+      $gender = mysqli_real_escape_string($con, $_POST['gender']);
+      $birthday = mysqli_real_escape_string($con, $_POST['birthday']);
+      $civilstatus = mysqli_real_escape_string($con, $_POST['civilstatus']);
+      $email = mysqli_real_escape_string($con, $_POST['email']);
+      $phone = mysqli_real_escape_string($con, $_POST['phone']);
+      $user_type = mysqli_real_escape_string($con, $_POST['role']);
+      
+      // Generate a random password
+      $new_password = substr(md5(microtime()), rand(0, 26), 8);
+      $password = md5($new_password);
+      $user_status = '1';
+      
+      $query = "INSERT INTO `user`(`fname`, `mname`, `lname`, `gender`, `birthday`, `civil_status`, `email`, `phone`, `password`, `user_type_id`, `user_status_id`) VALUES ('$fname','$mname','$lname','$gender','$birthday','$civilstatus','$email','$phone','$password','$user_type','$user_status')";
+      
+      $query_run = mysqli_query($con, $query);
+      
+      if ($query_run) {
+         $output = array('status' => "User added successfully", 'alert' => "success");
+      } else {
+         $output = array('status' => "User was not added", 'alert' => "error");
+      }
+      
+      echo json_encode($output);
    }
-
-   $stmt->bindValue(':limit', (int)$row, PDO::PARAM_INT);
-   $stmt->bindValue(':offset', (int)$rowperpage, PDO::PARAM_INT);
-   $stmt->execute();
-   $empRecords = $stmt->fetchAll();
-
-   $data = array();
-
-   foreach ($empRecords as $row) {
-      $data[] = array(
-         "user_id"=>$row['user_id'],
-         "fname"=>$row['fname'] . ' '. $row['mname']. $row['lname'],
-         "gender"=>$row['gender'],
-         "birthday"=>$row['birthday'],
-         "civil_status"=>$row['civil_status'],
-         "email"=>$row['email'],
-         "phone"=>$row['phone'],
-         "user_type_id"=>$row['user_type_id'],
-         "user_status_id"=>$row['user_status_id']
-      );
+   // -------------------------------- Edit User -------------------------------- //
+   if (isset($_POST["edit_user"])) {
+      // Include your database connection code here (e.g., $con)
+      
+      // Validate and sanitize user inputs
+      $fname = mysqli_real_escape_string($con, $_POST['fname']);
+      $mname = mysqli_real_escape_string($con, $_POST['mname']);
+      $lname = mysqli_real_escape_string($con, $_POST['lname']);
+      $gender = mysqli_real_escape_string($con, $_POST['gender']);
+      $birthday = mysqli_real_escape_string($con, $_POST['birthday']);
+      $civilstatus = mysqli_real_escape_string($con, $_POST['civilstatus']);
+      $email = mysqli_real_escape_string($con, $_POST['email']);
+      $phone = mysqli_real_escape_string($con, $_POST['phone']);
+      $user_type = mysqli_real_escape_string($con, $_POST['role']);
+      $user_status = mysqli_real_escape_string($con, $_POST['status']);
+      
+      $query = "UPDATE `user` SET `fname` = $fname, `mname` = $mname, `lname` = $lname, `gender` = $gender, `birthday` = $birthday, `civil_status` = $civilstatus, `email` = $email, `phone` = $phone, `user_type_id` = $user_type, `user_status_id` = $user_status";
+      
+      $query_run = mysqli_query($con, $query);
+      
+      if ($query_run) {
+         $output = array('status' => "User added successfully", 'alert' => "success");
+      } else {
+         $output = array('status' => "User was not added", 'alert' => "error");
+      }
+      
+      echo json_encode($output);
    }
-
-   // Response
-   $response = array(
-      "draw" => intval($draw),
-      "iTotalRecords" => $totalRecords,
-      "iTotalDisplayRecords" => $totalRecordwithFilter,
-      "aaData" => $data
-   );
-
-   echo json_encode($response);
+?>
--- admin/home/user.php
+++ admin/home/user.php
@@ -18,7 +18,7 @@
         </nav>
         <div class="card mb-4">
             <div class="card-header text-white bg-teal">Users Table
-                <button class="btn btn-primary btn-icon-split btn-sm float-end" data-bs-toggle="modal" data-bs-target="#add_user"> 
+                <button class="btn btn-primary btn-icon-split btn-sm float-end" data-bs-toggle="modal" data-bs-target="#btn_add_user"> 
                     <span class="icon text-white">
                         <i class="fas fa-user-plus"></i> Add User Account
                     </span>
@@ -49,13 +49,16 @@
                                 'serverSide': true,
                                 'serverMethod': 'post',
                                 'ajax': {
-                                    'url':'ajax.php'
+                                    'url':'ajax.php',
+                                    'data': function (data) {
+                                        data.user_list = "1"; // Include the parameter in the AJAX request
+                                    }
                                 },
                                 'columns': [
                                     { data: 'user_id', className: 'text-left' },
                                     { data: 'fname', className: 'text-left' },
                                     { data: 'gender', className: 'text-left' },
-                                    { data: 'birthday', className: 'text-left' },
+                                    { data: 'newbirthday', className: 'text-left' },
                                     { data: 'civil_status', className: 'text-left' },
                                     { data: 'email', className: 'text-left' },
                                     { data: 'phone', className: 'text-left' },
@@ -67,7 +70,9 @@
                                             return '<div class="row d-flex" style="justify-content:space-evenly;">'+
                                             '<button class="btn btn-datatable btn-icon btn-transparent-dark"><i class="fa fa-eye"></i></button>'+'<button class="btn btn-datatable btn-icon btn-transparent-dark"><i class="fa fa-edit"></i></button>'+'<button class="btn btn-datatable btn-icon btn-transparent-dark"><i class="fa fa-trash"></i></button>'+
                                             '</div>';
-                                        }
+                                        },
+                                        searchable: false, // Exclude from search
+                                        orderable: false   // Exclude from sorting
                                     }
                                 ]
                             });
@@ -78,16 +83,16 @@
         </div>
     </div>
 </main>
-<!-- Modal -->
-<div class="modal fade" id="add_user" data-bs-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="add_userLabel" aria-hidden="true">
+<!-- Modal for Add user -->
+<div class="modal fade" id="btn_add_user" data-bs-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="add_userLabel" aria-hidden="true">
     <div class="modal-dialog d-grid" role="document" style="justify-items: center;">
         <div class="modal-content" style="width:175%">
             <div class="modal-header card-header">
                 <h5 class="modal-title" id="add_userLabel">Add user</h5>
                 <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <form action="usercode.php" method="POST" enctype="multipart/form-data" >  
+            <form id="add_user_form" method="POST" enctype="multipart/form-data" > 
+                <div class="modal-body"> 
                     <div class="card mb-4">
                         <div class="card-header bg-teal">
                             <h5 class="text-white"><i class="far fa-user"></i> User information</h5>
@@ -164,12 +169,144 @@
                             </div>
                         </div>
                     </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button class="btn btn-primary" type="button"><i class="fa fa-plus"></i> Add</button>
-            </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary" id="add_user" type="button"><i class="fa fa-plus mr-1" style="margin-right:0.3rem"></i>Add</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
+<!-- Modal for Edit user -->
+<div class="modal fade" id="btn_add_user" data-bs-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="add_userLabel" aria-hidden="true">
+    <div class="modal-dialog d-grid" role="document" style="justify-items: center;">
+        <div class="modal-content" style="width:175%">
+            <div class="modal-header card-header">
+                <h5 class="modal-title" id="add_userLabel">Add user</h5>
+                <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id="add_user_form" method="POST" enctype="multipart/form-data" > 
+                <div class="modal-body"> 
+                    <div class="card mb-4">
+                        <div class="card-header bg-teal">
+                            <h5 class="text-white"><i class="far fa-user"></i> User information</h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="row"> 
+                                <div class="col-md-4 mb-3">
+                                    <label for="fname" class="required">First Name</label>
+                                    <input required placeholder="Enter First Name" type="text" id="fname" name="fname" class="form-control">
+                                    <div id="fname-error"></div>
+                                </div> 
+                            
+                                <div class="col-md-4 mb-3">
+                                    <label for="mname">Middle Name</label>
+                                    <input placeholder="Enter Middle Name" type="text" id="mname" name="mname" class="form-control">
+                                    <div id="mname-error"></div>
+                                </div>
+
+                                <div class="col-md-4 mb-3">
+                                    <label for="lname" class="required">Last Name</label>
+                                    <input required placeholder="Enter Last Name" type="text" id="lname" name="lname" class="form-control">
+                                    <div id="lname-error"></div>
+                                </div>
+
+                                <div class="col-md-4 mb-3">
+                                    <label for="gender" class="required">Gender</label>
+                                    <select id="gender" name="gender" required class="form-control">
+                                        <option value="" selected>Select Gender</option>
+                                        <option value="Male">Male</option>
+                                        <option value="Female">Female</option>
+                                    </select>
+                                    <div id="gender-error"></div>
+                                </div>
+
+                                <div class="col-md-4 mb-3">
+                                    <label for="birthday" class="required">Date of Birth</label>
+                                    <input required class="form-control" id="birthday" name="birthday" placeholder="MM/DD/YYY" type="date"/>
+                                    <div id="birthday-error"></div>
+                                </div>
+
+                                <div class="col-md-4 mb-3">
+                                    <label for="civilstatus" class="required">Civil Status</label>
+                                    <select id="civilstatus" name="civilstatus" required class="form-control">
+                                        <option value="" selected>Select Civil Status</option>
+                                        <option value="Single">Single</option>
+                                        <option value="Married">Married</option>
+                                        <option value="Widowed">Widowed</option>
+                                        <option value="Separated">Separated</option>
+                                    </select>
+                                    <div id="civilstatus-error"></div>
+                                </div>
+
+                                <div class="col-md-4 mb-3">
+                                    <label for="email" class="required">Email</label>
+                                    <input required placeholder="Enter Email" type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" class="form-control" id="email-input">
+                                    <div id="email-error"></div>
+                                </div>
+                            
+                                <div class="col-md-4 mb-3">
+                                    <label for="phone" class="required">Phone Number</label>
+                                    <input required placeholder="Enter Phone Number" type="text" name="phone" pattern="09[0-9]{9}" maxlength="11" class="form-control" id="phone-input">
+                                    <div id="phone-error"></div>
+                                </div>
+
+                                <div class="col-md-4 mb-3">
+                                    <label for="role" class="required">Role</label>
+                                    <select id="role" name="role" required class="form-control">
+                                        <option value="" selected>Select Role</option>
+                                        <option value="1">Admin</option>
+                                        <option value="2">Staff</option>
+                                    </select>
+                                    <div id="role-error"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary" id="add_user" type="button"><i class="fa fa-plus mr-1" style="margin-right:0.3rem"></i>Add</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#add_user').click(function() {
+            var formData = new FormData($('#add_user_form')[0]);
+            formData.append('add_user', '1'); // Identifier
+            $.ajax({
+                url: "ajax.php",
+                method: "POST",
+                data: formData,
+                dataType: "json",
+                contentType: false,
+                cache: false,
+                processData: false,
+                beforeSend: function() {
+                    $('#add_user').attr('disabled', 'disabled');
+                },
+                success: function(data) {
+                    swal({
+                        title: "Notice",
+                        text: data.status,
+                        icon: data.alert,
+                        button: true
+                    }).then(function() {
+                        $('#btn_add_user').modal('hide');
+                        $('#add_user_form')[0].reset();
+                        $('#add_user').removeAttr('disabled');
+                        // Update the DataTable
+                        var dataTable = $('#datatablesSimple1').DataTable();
+                        dataTable.draw(); // Redraw the DataTable
+                    });
+                },
+                error: function(xhr, textStatus, errorThrown) {
+                    console.error(errorThrown);
+                }
+            });
+        });
+    });
+</script>
 <?php include ('../includes/footer.php'); ?>
