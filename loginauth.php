951e0d24231e5781d2d8ec7cf8f0e9e8e55c67c8
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-11-04 17:32:57 +0800

    add two step authentication

---
+++ loginauth.php
@@ -0,0 +1,103 @@
+<?php include ('db_conn.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="<?= $system['description'] ?>">
+        <meta name="keywords" content="<?= $system['keywords'] ?>">
+        <meta name="author" content="<?= $system['author'] ?>">
+        <!-- Title Page -->
+        <title><?= $system['shortname'] ?> | Two Step Authentication</title>
+        <!-- Favicons -->
+        <link rel="shortcut icon" href="<?php echo base_url ?>assets/files/system/<?= $system['icon'] ?>" type="image/x-icon">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url ?>assets/files/system/<?= $system['icon'] ?>">
+        <link rel="apple-touch-icon" sizes="152x152" href="<?php echo base_url ?>assets/files/system/<?= $system['icon'] ?>">
+        <!-- Custom fonts for this template-->
+        <link href="<?php echo base_url ?>assets/vendor/font-awesome/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+        <!-- Loader CSS -->
+        <link href="<?php echo base_url ?>assets/css/loader.css" rel="stylesheet">
+        <!-- Custom styles for this template-->
+        <link href="<?php echo base_url ?>assets/css/sb-admin-2.css" rel="stylesheet">
+        <!-- Styles for center login -->
+        <style>
+            body{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+                background-color: #f8f9fc;
+            }
+        </style>
+    </head>
+    <body class="bg-gradient-primary">
+        <!-- Loading Screen -->
+        <div class="noprint-scroll" id="loading">
+            <img id="loading-image" src="<?php echo base_url ?>assets/files/system/loading.gif" alt="Loading" />
+        </div>
+        <div class="container">
+            <!-- Outer Row -->
+            <div class="row justify-content-center">
+                <div class="col-xl-10 col-lg-12 col-md-9">
+                    <div class="card o-hidden border-0 shadow-lg my-5">
+                        <div class="card-body p-0">
+                            <!-- Nested Row within Card Body -->
+                            <div class="row">
+                                <div class="col-lg-6 d-none d-lg-block bg-login-image" style="background: url('<?php echo base_url ?>assets/files/system/<?= $system['logo'] ?>'); background-position:center; background-size:cover; background-repeat:no-repeat;"></div>
+                                <div class="col-lg-6 bg-light">
+                                    <div class="p-3">
+                                        <div class="text-center">
+                                            <h1 class="h4 text-gray-900"><?= $system['name'] ?></h1>
+                                            <hr style="margin-top:5px !important; margin-bottom:5px !important">
+                                            <h1 class="h4 text-gray-900 mb-4">Two Step Authentication</h1>
+                                            <h5 class="text-gray-900">Open your email address and enter the code for <?= $system['shortname'] ?></h5>
+                                        </div>
+                                        <form class="user" action="loginauthcode.php" method="POST">
+                                            <?php
+                                                $user_email = $_SESSION['auth_user']['user_email'];
+                                                $username = $_SESSION['auth_user']['user_name'];
+                                            ?>
+                                            <div class="form-group">
+                                                <label for="user_email" class="">Your Email</label>
+                                                <input type="text" class="form-control form-control-user" id="user_email" value="<?=htmlspecialchars($user_email);?>" disabled>
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="text" id="verify_code" name="verify_code" class="form-control form-control-user" placeholder="Enter 6 digit code" required>
+                                            </div>
+                                            <button type="submit" name="verify_btn" id="verifyButton" class="btn btn-primary btn-user btn-block">Verify</button>
+                                        </form>
+                                        <hr>
+                                        <div class="text-center">
+                                            <a class="small text-decoration-none" href="login">Not <?=htmlspecialchars($username);?>?</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Sweetalert JavaScript -->
+        <script src="<?php echo base_url ?>assets/js/sweetalert.js"></script>
+        <!-- Bootstrap core JavaScript-->
+        <script src="<?php echo base_url ?>assets/vendor/jquery/jquery.min.js"></script>
+        <script src="<?php echo base_url ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- Core plugin JavaScript-->
+        <script src="<?php echo base_url ?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+        <!-- Loader JS File -->
+        <script src="<?php echo base_url ?>assets/js/loader.js"></script>
+        <!-- Validations forms -->
+        <script src="<?php echo base_url ?>assets/js/jquery-3.2.1.min.js"></script>
+        <script src="<?php echo base_url ?>assets/js/underscore-min.js"></script>
+        <!-- Restrictions forms -->
+        <script src="<?php echo base_url ?>assets/js/disable-key.js"></script>
+        <!-- Script for save last inputed Email or Password -->
+
+        <!-- Sweetalert message popup -->
+        <?php include ('message.php'); ?> 
+    </body>
+</html>
--- logincode.php
+++ logincode.php
@@ -1,5 +1,9 @@
 <?php
     include('db_conn.php');
+    // PHP Mailer
+    include ("./assets/vendor/PHPMailer/PHPMailerAutoload.php");
+    include ("./assets/vendor/PHPMailer/class.phpmailer.php");
+    include ("./assets/vendor/PHPMailer/class.smtp.php");
 
     if(isset($_POST['login_btn'])){
         $email_phone = mysqli_real_escape_string($con, $_POST['email_phone']);
@@ -14,38 +18,74 @@
                 $full_name = $data['fname'].' '.$data['lname'];
                 $role_as = $data['user_type_id'];
                 $user_email = $data['email'];
+                $twostepauth = $data['second_auth'];
             }
+            if($twostepauth == '1'){
+                $randomCode = strval(rand(100000, 999999)); // random code generator
+                $validity = date('Y-m-d H:i:s', strtotime('+15 minutes'));
 
-            $date = date('Y-m-d H:i:s');
-            $login_success = "Login";
-            $login_success_log = "success using email and password";
-            mysqli_query($con,"INSERT INTO user_log (user_id, type, log, date) values('".$user_id."','".$login_success."','".$login_success_log."','$date')");
+                $stmt = "INSERT INTO `twostepauth`(`user_id`, `code`, `validity`) VALUES ('$user_id','$randomCode','$validity')";
+                $stmt_run = mysqli_query($con, $stmt);
+                if ($stmt_run){
+                    // Email sender PHP
+                    $name = htmlentities($system['name']);
+                    $subject = htmlentities('Two Step Authentication Code - '. $system['name']);
+                    $message = nl2br("Dear $fname \r\n \r\n This is your two step authentication login code \r\n \r\n Code: ".$randomCode." \r\n\r\n This code will valid for 15 minutes. \r\n \r\n Thanks, \r\n ".$system['name']);
+                    //PHP Mailer Gmail
+                    $mail = new PHPMailer();
+                    $mail->IsSMTP();
+                    $mail->SMTPAuth = true;
+                    $mail->SMTPSecure = 'TLS/STARTTLS';
+                    $mail->Host = 'smtp.gmail.com'; // Enter your host here
+                    $mail->Port = '587';
+                    $mail->IsHTML();
+                    $mail->Username = emailuser; // Enter your email here
+                    $mail->Password = emailpass; //Enter your passwrod here
+                    $mail->setFrom($user_email, $name);
+                    $mail->addAddress($user_email);
+                    $mail->Subject = $subject;
+                    $mail->Body = $message;
+                    $mail->send();
 
-            $_SESSION['auth'] = true;
-            $_SESSION['auth_role'] = "$role_as";
-            $_SESSION['auth_user'] = [
-                'user_id' =>$user_id,
-                'user_name' =>$full_name,
-                'user_email' =>$user_email,
-            ];
+                    $_SESSION['auth_user'] = ['user_email' =>$user_email, 'user_name' =>$full_name,];
+                    header("Location: " . base_url . "loginauth");
+                } else{
+                    $_SESSION['status'] = "There is something error Please try again";
+                    $_SESSION['status_code'] = "error";
+                    header("Location: " . base_url . "login");
+                }
+            } else {
+                $date = date('Y-m-d H:i:s');
+                $login_success = "Login";
+                $login_success_log = "success using email and password";
+                mysqli_query($con,"INSERT INTO user_log (user_id, type, log, date) values('".$user_id."','".$login_success."','".$login_success_log."','$date')");
 
-            if( $_SESSION['auth_role'] == '1'){
-                $_SESSION['status'] = "Welcome $full_name!";
-                $_SESSION['status_code'] = "success";
-                header("Location: " . base_url . "admin");
-                exit(0);
-            }
-            elseif( $_SESSION['auth_role'] == '2'){
-                $_SESSION['status'] = "Welcome $full_name!";
-                $_SESSION['status_code'] = "success";
-                header("Location: " . base_url . "staff");
-                exit(0);
-            }
-            elseif( $_SESSION['auth_role'] == '3'){
-                $_SESSION['status'] = "Welcome $full_name!";
-                $_SESSION['status_code'] = "success";
-                header("Location: " . base_url . "client");
-                exit(0);
+                $_SESSION['auth'] = true;
+                $_SESSION['auth_role'] = "$role_as";
+                $_SESSION['auth_user'] = [
+                    'user_id' =>$user_id,
+                    'user_name' =>$full_name,
+                    'user_email' =>$user_email,
+                ];
+            
+                if( $_SESSION['auth_role'] == '1'){
+                    $_SESSION['status'] = "Welcome $full_name!";
+                    $_SESSION['status_code'] = "success";
+                    header("Location: " . base_url . "admin");
+                    exit(0);
+                }
+                elseif( $_SESSION['auth_role'] == '2'){
+                    $_SESSION['status'] = "Welcome $full_name!";
+                    $_SESSION['status_code'] = "success";
+                    header("Location: " . base_url . "staff");
+                    exit(0);
+                }
+                elseif( $_SESSION['auth_role'] == '3'){
+                    $_SESSION['status'] = "Welcome $full_name!";
+                    $_SESSION['status_code'] = "success";
+                    header("Location: " . base_url . "client");
+                    exit(0);
+                }
             }
         }
         else {
